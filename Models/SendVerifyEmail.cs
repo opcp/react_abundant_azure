f390da20b533b93a46e81138bf8697b85484705a
Shan <a24215188@gmail.com>
2021-09-22 22:19:01 +0800

    驗證電子郵件 Class

---
+++ Models/SendVerifyEmail.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace react_abundant_azure.Models
+{
+    public class SendVerifyEmail
+    {
+        private readonly abundantContext _context = new abundantContext();
+        private readonly string auth = Guid.NewGuid().ToString();
+
+        public void Authorization(int memberId)
+        {
+            DateTime LimitTime = DateTime.Now.AddMinutes(10);
+
+            string EmailSendTo = _context.Members.Where((i) => i.Id == memberId).Select((i) => i.Email).FirstOrDefault().ToString();
+
+            _context.VerifyMails.Add(new VerifyMail
+            {
+                VerifyString = auth,
+                EnableTime = LimitTime,
+                MemberId = memberId
+            });
+
+            _context.SaveChanges();
+
+            SendEamil(EmailSendTo, LimitTime);
+        }
+
+        private void SendEamil(string EmailSendTo, DateTime LimitTime)
+        {
+            try
+            {
+                using (MailMessage mail = new MailMessage())
+                {
+                    string link = "https://localhost:44374/verify?AccessToken=" + auth;
+                    mail.From = new MailAddress("abundantserviceiceland@gmail.com");
+                    mail.To.Add(EmailSendTo);
+                    mail.Subject = "Abundant Verify Mail";
+                    mail.Body = "<div>" +
+                        "<h3> Click link to verify </h3>" +
+                             "<div style=\"width: fit-content; background-color: rgba(97, 97, 97, 0.3)\">" +
+                             "<h3>" +
+                             $"<a style=\"text-decoration: none\" href = \"{link}\">{link}</a>" +
+                             "</h3></div>"
+                       + $"<h3> The link will expire at {LimitTime} </h3>" + "</div>";
+
+                    mail.IsBodyHtml = true;
+
+                    using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        smtp.Credentials = new NetworkCredential("abundantserviceiceland@gmail.com", "~Abundant2421");
+                        smtp.EnableSsl = true;
+                        smtp.Send(mail);
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Models/VerifyMail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace react_abundant_azure.Models
+{
+    public partial class VerifyMail
+    {
+        public int ListNumber { get; set; }
+        public string VerifyString { get; set; }
+        public int MemberId { get; set; }
+        public DateTime EnableTime { get; set; }
+
+        public virtual Member Member { get; set; }
+    }
+}
